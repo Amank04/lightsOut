1bc9dbd9ba89bf17497ef9fc2aeb60239bdf6f45
Aditya Singh <122855095+adityasingh151@users.noreply.github.com>
2024-05-09 20:07:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1168,7 +1168,7 @@ passport.use(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/userProfile",
+        callbackURL: "https://lightsout-w861.onrender.com/auth/google/userProfile",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       },
       async (accessToken, refreshToken, profile, cb) => {
